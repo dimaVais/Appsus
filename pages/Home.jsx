1665fcd9bb15776291dd140e7f95ab32c9b1b341
Asaf Dulman <asafdulman@gmail.com>
2020-08-29 17:34:26 +0300

    image check 5

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -18,14 +18,14 @@ export class Home extends React.Component {
                 </div>
                 <div className="home-img-box"> 
                 <div className="home-img-mail-box">
-                   <Link to={'/mail/list'}><img className="home-img-mail"  src="./general-assets/img/home-mail1.png" alt="mail"/></Link>
+                   <Link to={'/mail/list'}><img className="home-img-mail"  src="../general-assets/img/home-mail1.png" alt="mail"/></Link>
                    
                 </div>
                 <div className="home-img-keep-box">
-                <Link to={'/keep'}>   <img className="home-img-keep" src="./general-assets/img/home-note2.jpg" alt="keep"/></Link>
+                <Link to={'/keep'}>   <img className="home-img-keep" src="general-assets/img/home-note2.jpg" alt="keep"/></Link>
                 </div>
                 <div className="home-img-book-box">
-                <Link to={'/book'}> <img className="home-img-book" src="./general-assets/img/home-book.png" alt="books"/></Link>
+                <Link to={'/book'}> <img className="home-img-book" src="../general-assets/img/home-book.png" alt="books"/></Link>
                 </div>
                 </div>
             </section>
