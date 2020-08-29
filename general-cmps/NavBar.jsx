38321294f1dd2f42105585c50985151c3f76d21d
Asaf Dulman <asafdulman@gmail.com>
2020-08-29 17:29:35 +0300

    images check

--- general-cmps/NavBar.jsx
+++ general-cmps/NavBar.jsx
@@ -10,10 +10,10 @@ function _NavBar({toggleMenu}) {
         <nav>
 
             <div className="main-nav">
-                <NavLink exact className="navLink" onClick={() => {toggleMenu()} }  to="/"><img className="nav-bar-home-img" src="../general-assets/img/home2.png" alt="Home" /></NavLink>
-                <NavLink className="navLink" onClick={() => {toggleMenu()} } to="/mail/list"><img className="nav-bar-mail-img" src="../general-assets/img/mail1.png" alt="Mail" /></NavLink>
-                <NavLink className="navLink" onClick={() => {toggleMenu()} } to="/keep"><img className="nav-bar-keep-img" src="../general-assets/img/keep.png" alt="Keep" /></NavLink>
-                <NavLink className="navLink" onClick={() => {toggleMenu()} } to="/Book"><img className="nav-bar-book-img" src="../general-assets/img/book.png" alt="Book" /></NavLink>
+                <NavLink exact className="navLink" onClick={() => {toggleMenu()} }  to="/"><img className="nav-bar-home-img" src="./general-assets/img/home2.png" alt="Home" /></NavLink>
+                <NavLink className="navLink" onClick={() => {toggleMenu()} } to="/mail/list"><img className="nav-bar-mail-img" src="./general-assets/img/mail1.png" alt="Mail" /></NavLink>
+                <NavLink className="navLink" onClick={() => {toggleMenu()} } to="/keep"><img className="nav-bar-keep-img" src="./general-assets/img/keep.png" alt="Keep" /></NavLink>
+                <NavLink className="navLink" onClick={() => {toggleMenu()} } to="/Book"><img className="nav-bar-book-img" src="./general-assets/img/book.png" alt="Book" /></NavLink>
             </div>
         </nav>
     )
--- pages/Home.jsx
+++ pages/Home.jsx
@@ -19,12 +19,13 @@ export class Home extends React.Component {
                 <div className="home-img-box"> 
                 <div className="home-img-mail-box">
                    <Link to={'/mail/list'}><img className="home-img-mail"  src="../general-assets/img/home-mail1.png" alt="mail"/></Link>
+                   
                 </div>
                 <div className="home-img-keep-box">
-                <Link to={'/keep'}>   <img className="home-img-keep" src="../general-assets/img/home-note2.jpg" alt=""/></Link>
+                <Link to={'/keep'}>   <img className="home-img-keep" src="../general-assets/img/home-note2.jpg" alt="keep"/></Link>
                 </div>
                 <div className="home-img-book-box">
-                <Link to={'/book'}> <img className="home-img-book" src="../general-assets/img/home-book.png" alt=""/></Link>
+                <Link to={'/book'}> <img className="home-img-book" src="../general-assets/img/home-book.png" alt="books"/></Link>
                 </div>
                 </div>
             </section>
