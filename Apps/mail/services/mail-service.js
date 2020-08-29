91ca252f36c73ce8f8f34f140100c9e81a17790a
Asaf Dulman <asafdulman@gmail.com>
2020-08-29 19:37:02 +0300

    5

--- Apps/mail/services/mail-service.js
+++ Apps/mail/services/mail-service.js
@@ -26,9 +26,17 @@ var startMails = [
     },
     {
         id: utilService.makeId(),
-        from: 'Alon Barak',
+        from: 'Austin Powers',
         subject: 'Trip Plans',
         body: 'Hey D,\nPlease see below the new trip plan to Africa!\nLooking forward getting your response\nThanks',
+        isRead: false,
+        sentAt: new Date(Date.now()).toLocaleString()
+    },
+    {
+        id: utilService.makeId(),
+        from: 'Jules Winnfield',
+        subject: 'Did You Read the Bible Brett?',
+        body: 'The path of the righteous man is beset of all sides by the iniquities of the selfish and the tyranny of evil me. Blessed is he who, in the name of the charity and good will, shepherds the weak through the valley of darkness, for he is truly his brothers keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who attempt to poison and destroy my brothers. And you will know my name is the Lord when I lay my vengeance upon thee.',
         isRead: false,
         sentAt: new Date(Date.now()).toLocaleString()
     },
@@ -52,10 +60,44 @@ var startMails = [
         id: utilService.makeId(),
         from: 'Alex Turner',
         subject: 'Why’d You Only Call Me When You’re High ?',
-        body: 'Why’d You Only Call Me When You’re High ?',
+        body: 'Tells me its home time But Im not finished use youre not by my side And as I arrived I thought I saw you leaving Carrying your shoes   Decided that once again I was just dreaming Of bumping into you',
+        isRead: false,
+        sentAt: new Date(Date.now()).toLocaleString()
+    },
+    {
+        id: utilService.makeId(),
+        from: 'DropBox',
+        subject: 'Asaf and 43 others made changes in you shared folder',
+        body: 'Hi Dima, Everybody is changing their projects and you have to keep up! Thanks',
+        isRead: false,
+        sentAt: new Date(Date.now()).toLocaleString()
+    },
+    {
+        id: utilService.makeId(),
+        from: 'Quentin Tarantino',
+        subject: 'The Bonnie Situation',
+        body: 'Tarantino and Avary were contemplating writing a trilogy of films about the criminal underworld. As they continued to jam on scenes together, they realized that an ensemble film told in chapters might be more interesting.',
+        isRead: false,
+        sentAt: new Date(Date.now()).toLocaleString()
+    },
+    
+    {
+        id: utilService.makeId(),
+        from: 'Marsellus Wallace',
+        subject: 'Deal With Butch',
+        body: 'Fuck pride. Pride only hurts, it never helps',
+        isRead: false,
+        sentAt: new Date(Date.now()).toLocaleString()
+    },
+    {
+        id: utilService.makeId(),
+        from: 'Jules Winnfield',
+        subject: 'Breakfast at Tiffani',
+        body: 'If my answers frighten you then you should cease asking scary questions',
         isRead: false,
         sentAt: new Date(Date.now()).toLocaleString()
     }
+
 ]
 
 var mails = storageService.loadFromStorage(MAIL_KEY) || startMails;
